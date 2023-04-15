1a8552f207284705dbf4c1f32d0f299b1a4c8bdc
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-04-15 18:08:41 +0100

    MVP

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,43 @@
-use std::{process::Command, thread::sleep, time::Duration};
+use std::{process::Command};
+use console::{Term, Key};
 
 const DIR: &str = ".";
 
 fn main() {
     let commits = get_commits();
+    
+    let stdout = Term::buffered_stdout();
+    
+    let mut index = 0;
 
-    for commit in commits {
-        checkout_commit(commit);
-        sleep(Duration::from_secs(1));
+    loop {
+        if let Ok(character) = stdout.read_key() {
+            match character {
+                Key::ArrowLeft => {
+                    if index > 1 {
+                        index -= 1;
+                        checkout_commit(&commits[index]);
+                    }
+                },
+                Key::ArrowRight => {
+                    if index < commits.len() -1 {
+                        index += 1;
+                        checkout_commit(&commits[index]);
+                    }
+                },
+                _ => break 
+            }
+        }
     }
 }
 
-fn checkout_commit(commit: String) {
+fn checkout_commit(commit: &String) {
     Command::new("git")
         .arg("-C")
         .arg(DIR)
         .arg("checkout")
         .arg(commit)
-        .spawn()
+        .status()
         .unwrap();
 }
 
