d9e7d6e3b385a0495eb7658a68be3d507834765c
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2023-04-17 12:36:36 +0100

    Panic when command fails

--- src/git.rs
+++ src/git.rs
@@ -1,14 +1,21 @@
 use crate::Args;
 use std::{
     io,
-    process::{Command, ExitStatus},
+    process::{Command, Stdio},
 };
 
-pub fn checkout_target(target: &String, args: &Args) -> Result<ExitStatus, io::Error> {
-    base_git_command(&args)
+pub fn checkout_target(target: &String, args: &Args) -> Result<(), io::Error> {
+    let status = base_git_command(&args)
         .args(["-c", "advice.detachedHead=false"])
         .args(["checkout", target])
-        .status()
+        .stdout(Stdio::inherit())
+        .status()?;
+
+    if !status.success() {
+        panic!("checkout failed");
+    }
+
+    Ok(())
 }
 
 pub fn get_commits(args: &Args) -> Result<Vec<String>, io::Error> {
@@ -17,6 +24,10 @@ pub fn get_commits(args: &Args) -> Result<Vec<String>, io::Error> {
         .arg(&args.target)
         .arg("--reverse")
         .output()?;
+
+    if !output.status.success() {
+        panic!("failed to load commits");
+    }
 
     let raw = String::from_utf8_lossy(&output.stdout);
 
